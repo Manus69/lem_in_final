c2be45602e1d3584d0fcb62f2d1dca3b0e49e59e
Lorimer Caesar <lcaesar@mi-a5.21-school.ru>
2020-02-16 17:15:59 +0300

    ?

--- will_it_work/run.c
+++ will_it_work/run.c
@@ -6,7 +6,7 @@
 /*   By: lcaesar <lcaesar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/15 16:35:00 by lcaesar           #+#    #+#             */
-/*   Updated: 2020/02/16 16:13:22 by lcaesar          ###   ########.fr       */
+/*   Updated: 2020/02/16 17:15:22 by lcaesar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ static void	initialize_stuff(t_colony *colony)
 {
 	colony->ants = spawn_all_ants(colony->start, colony->number_of_ants);
 	colony->current_flow = get_flow_paths(colony, 0);
-	// colony->current_flow = new_path_list(new_path(naive_bfs(colony->start, colony->finish, 0)));
 	if (!colony->current_flow)
 	{
 		write(1, ERROR_MSG, ft_strlen(ERROR_MSG));
@@ -96,55 +95,6 @@ static void	block_and_go(t_colony *colony)
 	block_pivots(colony->pivot_nodes, 0);
 	go_forth(colony);
 }
-
-/*
-check if the input implementation is still retarded;
-what about move description length?
-go through the assignment and the check list;
-
-bus error on several arguments on stdin ?
-reads stdin when ./lem-in [nonexistant file];
-investigate extremely low move counts;
-remove -g flag;
-are header dependancies set?
-
-test the pivot placing strategies;
-play around with path initialization;
-superimpose_mk2() does not remove edges;
-
-TESTS:
-200 sample size:
--gfp, cut off, 0-1-2 superimpose:
-⤷ Time average: 0.289s
-⤷ Average: -0.26
-⤷ Min: -7
-⤷ Max: 4
--g1p, cut off, 0-1-2 superimpose:
- ⤷ Time average: 0.304s
-  ⤷ Average: -0.21
-  ⤷ Min: -7
-  ⤷ Max: 5
--gfp, no cut off, 0-1-2 superimpose:
- ⤷ Time average: 0.370s
-  ⤷ Average: -0.40
-  ⤷ Min: -18
-  ⤷ Max: 3
--gfp, cut off, 0-1-0 superimpose:
-⤷ Time average: 0.262s
-  ⤷ Average: -0.12
-  ⤷ Min: -32
-  ⤷ Max: 5
--gfp, no cut off, 0-1-0:
- ⤷ Time average: 0.333s
-  ⤷ Average: -0.16
-  ⤷ Min: -6
-  ⤷ Max: 5
--g1p, no cut off, 0-1-0:
-⤷ Time average: 0.324s
-  ⤷ Average: -0.18
-  ⤷ Min: -4
-  ⤷ Max: 5
-*/
 
 int			main(int arg_count, char **arg_values)
 {
